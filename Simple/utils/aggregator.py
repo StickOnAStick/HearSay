31fd036c253a19f49d6f4e58cfe844c3a7d706d3
kambati067 <ketanreddy.ambati@sjsu.edu>
2025-02-24 22:08:50 -0800

    fix issues in code

--- Simple/utils/aggregator.py
+++ Simple/utils/aggregator.py
@@ -20,7 +20,7 @@ class Aggregator:
 
     def aggregate(self):
         keywords: list[Keyword] = self.get_keywords()
-        optimal_k: int = self.find_optimal_k_clusters(keywords=keywords, k_min=1, k_max=len(keywords))
+        optimal_k: int = self.find_optimal_k_clusters(keywords=keywords, k_min=2, k_max=len(keywords)-1)
         cluster_keywords: list[list[Keyword]] = self.cluster_k_means(k=optimal_k, keywords=keywords)
         clusters: list[Cluster] = self.get_cluster_label(cluster_keywords)
         self.cluster_to_csv(clusters, filename="test")
@@ -50,8 +50,8 @@ class Aggregator:
         #Finds k with highest silhouette score
         for k in range(k_min, k_max+1):
             kmeans = KMeans(n_clusters=k, n_init=10, random_state=0)
-            labels = kmeans.fit(embeddings)
-            score = silhouette_score(embeddings, labels)
+            kmeans.fit(embeddings)
+            score = silhouette_score(embeddings, kmeans.labels_)
 
             if score>max_silhouette_score:
                 optimal_k, max_silhouette_score = k, score
