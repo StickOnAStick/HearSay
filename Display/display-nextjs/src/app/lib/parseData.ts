e70ade7f317fdccb03827f9d6dc6134732721b8b
StickOnAStick <StickOnAStick>
2025-05-15 19:07:11 -0700

    NOT FULL FIX: Updated display to work with new data type.

---
+++ Display/display-nextjs/src/app/lib/parseData.ts
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import Papa from 'papaparse';
+import { NextResponse } from 'next/server';
+
+export async function parseData(file_path){
+
+	const aggregatePath = path.join(process.cwd(), file_path);
+
+	try {
+		const aggregatedData = fs.readFileSync(aggregatePath, 'utf-8');
+		const parsedAggregate = Papa.parse(aggregatedData, {header:true}).data;
+		return parsedAggregate;
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+}
